a24808572f69a42691e3e57b2d52f377c487dc32
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-22 19:17:54 +0200

    Titre en variable PHP

--- utils.inc.php
+++ utils.inc.php
@@ -2,11 +2,11 @@
     function start_page ($title)
     {
         echo '<!DOCTYPE html> ' . PHP_EOL .
-             '<html lang=fr> ' . PHP_EOL .
+             '<html lang=fr style="height: 100%;"> ' . PHP_EOL .
              '<head>
                     <title>' . $title . '</title>
                   </head>
-                  <body>' . PHP_EOL;
+                  <body style="height: 100%;">' . PHP_EOL;
     };
     function end_page (){
         echo '</body>
