0e2e1706d1229e563a43ad975c3556cecf6a096b
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-23 17:44:13 +0200

    Nombre de connections

--- test-pass.php
+++ test-pass.php
@@ -26,6 +26,14 @@
                 $_SESSION['login'] = 'ok';
                 $_SESSION['id'] = $login;
                 $_SESSION['pwd'] = $pwd;
+                if (!($updateQuery = mysqli_query($dbLink, 'UPDATE user SET nbConnections = nbConnections + 1 WHERE login = \'' . $login . '\''))) {
+                    echo 'Erreur de requête<br/>';
+                    // Affiche le type d'erreur.
+                    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+                    // Affiche la requête envoyée.
+                    echo 'Requête : ' . $query . '<br/>';
+                    exit();
+                };
             }
         }
     } else {
